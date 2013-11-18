80fd091de29311a8c109db73f13955ee6dfc8bac
fr33jc <fr33jc@gmail.com>
2013-11-18 13:18:13 -0800

    Annotate attributes in ``bang.attributes``

--- bang/attributes/__init__.py
+++ bang/attributes/__init__.py
@@ -17,40 +17,68 @@
 """
 Constants for attribute names of the various resources.
 
-Just a place to stash more magic strings.
+This module contains the top-level config file attributes including those that
+are typically placed in ~/.bangrc.
 """
 from . import creds, server, secgroup, tags, database, ssh_key, loadbalancer
 
-# stack attributes
+#: The stack name.  Its value is used to tag servers and other cloud resources.
 NAME = 'name'
+
+#: The stack version.  Often, you need a global version of a stack in a
+#: playbook.  E.g. when a web client wants to query a web service for API
+#: compatibility, the playbooks could configure the web service to report this
+#: stack version.
 VERSION = 'version'
-LOGGING = 'logging'
+
+#: The ordered list of playbooks to run *after* provisioning the cloud
+#: resources.
 PLAYBOOKS = 'playbooks'
-STACK = 'stack'
-DEPLOYER_CREDS = 'deployer_credentials'
-KEY = 'key'
-SERVER_CLASS = 'server_class'
+
+#: The resource provider (e.g. ``aws``, ``hpcloud``).  Values for the
+#: ``provider`` attribute will be used to look up the appropriate
+#: :class:`~bang.providers.bases.Provider` subclass to use when instantiating
+#: the associated resource.
 PROVIDER = 'provider'
 
-# Like chicken fried chicken... this is a way to configure the name of the tag
-# in which the combined stack-role (a.k.a. *name*) will be stored.  By default,
-# unless this is specified directly in ~/.bangrc, the *name* value will be
-# assigned to a tag named "Name" (this is the default tag displayed in the AWS
-# management console).  I.e. using Bang defaults, the server named "bar" in the
-# stack named "foo" will have the following tags:
-#
-#     stack:  foo
-#     role:   bar
-#     Name:   foo-bar
-#
-# In some cases, admins may have other purposes for the "Name" tag.  If
-# ~/.bangrc were to have ``name_tag_name`` set to ``descriptor``, then the
-# server described above would have the following tags:
-#
-#     stack:       foo
-#     role:        bar
-#     descriptor:  foo-bar
-#
-# To prevent Bang from assigning the *name* value to a tag, assign an empty
-# string to the ``name_tag_name`` attribute in ~/.bangrc.
+#: A dict containing credentials for various cloud providers in which the keys
+#: can be any valid provider.  E.g.  ``aws``, ``hpcloud``.
+DEPLOYER_CREDS = 'deployer_credentials'
+
+#: The top-level key for logging-related configuration options.
+LOGGING = 'logging'
+
+#: Like chicken fried chicken... this is a way to configure the name of the tag
+#: in which the combined stack-role (a.k.a. *name*) will be stored.  By
+#: default, unless this is specified directly in ~/.bangrc, the *name* value
+#: will be assigned to a tag named "Name" (this is the default tag displayed
+#: in the AWS management console).  I.e. using Bang defaults, the server
+#: named "bar" in the stack named "foo" will have the following tags::
+#:
+#:     stack:  foo
+#:     role:   bar
+#:     Name:   foo-bar
+#:
+#: In some cases, admins may have other purposes for the "Name" tag.  If
+#: ~/.bangrc were to have ``name_tag_name`` set to ``descriptor``, then the
+#: server described above would have the following tags::
+#:
+#:     stack:       foo
+#:     role:        bar
+#:     descriptor:  foo-bar
+#:
+#: To prevent Bang from assigning the *name* value to a tag, assign an empty
+#: string to the ``name_tag_name`` attribute in ~/.bangrc.
 NAME_TAG_NAME = 'name_tag_name'
+
+#: This is a *derived* attribute that Bang provides for instance tagging, and
+#: for Ansible playbooks to consume.  It's a combination of the :attr:`NAME`
+#: and the :attr:`VERSION`.
+STACK = 'stack'
+
+KEY = 'key'
+
+#: This is a *derived* attribute that Bang provides for instance tagging, and
+#: for Ansible playbooks to consume.  It's a combination of the :attr:`NAME`
+#: and the :attr:`VERSION`.
+SERVER_CLASS = 'server_class'
